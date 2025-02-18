013e14dbbe1a9d325cdb522a28510fa24da209bc
gabriel H. <129757379+GabryelHenryque@users.noreply.github.com>
2025-02-18 20:47:25 -0300

    funfa as vezes
    ter que arrumar os bagulhos ali ó

--- test.js
+++ test.js
@@ -7,13 +7,13 @@ const questions = [
             "bonitão",
             "meio bonito"]
     },{
-        question:" é bonita?",
+        question:"Joyce é bonita?",
         alter_correct:"feia",
         alter_incorrect:[
             "fedida",
             "horrorosa",
             "um pouco"]
-    },
+    }, 
 ]
 
 //question[random].alter ==== [horrorosa,fedida,pouco,feia]
@@ -47,7 +47,7 @@ let newQuestions = () =>{
     i_quest = i_quest()
 
     //cria array atualizada
-    for (let _i = 0; _i < 2 ; _i++) {
+    for (let _i = 0; _i < (temp = questions.length <= 30 ? questions.length : 30); _i++) {
         //cria array
         const alter = []
         alter.push(questions[i_quest[_i]].alter_correct, ...questions[i_quest[_i]].alter_incorrect)
@@ -70,47 +70,105 @@ let newQuestions = () =>{
 
 const getId = (id) => document.getElementById(id);
 const container_quest = getId("container_quests")
+let correction = ""
 newQuestions().forEach( (e,i) => {
-
-    container_quest.insertAdjacentHTML ('afterbegin', `
+    container_quest.innerHTML += `
     <div class="container shadow-sm mb-4  bg-light ">
         <p class="fs-5"><span>${i + 1} - </span>${e.question}</p>
         <!--questoes-->
         
-        <div class="form-check ">
-            <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioA_${i}">
-            <label class="form-check-label" for="alterRadioA_${i}">A - ${e.alter[0]}
-            
-            </label>
-            </div>
-            <div class="form-check">
-            <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioB_${i}" >
-            <label class="form-check-label" for="alterRadioB_${i}">B - ${e.alter[1]}
-                
-            </label>
+            <div class="form-check" >
+                <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioA_${i}" value="${e.alter[0]} ">
+                <label class="form-check-label" for="alterRadioA_${i}">A - ${e.alter[0]} </label>
+                <label id="correctionA_${i}"></label>
             </div>
 
             <div class="form-check">
-            <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioC_${i}">
-            <label class="form-check-label" for="alterRadioC_${i}">C - ${e.alter[2]}
-            
-            </label>
+                <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioB_${i}" value="${e.alter[1]}">
+                <label class="form-check-label" for="alterRadioB_${i}">B - ${e.alter[1]}</label>
+                <label id="correctionB_${i}"></label>
             </div>
-            <div class="form-check">
-            <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioD_${i}" >
-            <label class="form-check-label" for="alterRadioD_${i}">D - ${e.alter[3]}
 
-            </label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioC_${i}" value="${e.alter[2]}">
+                <label class="form-check-label" for="alterRadioC_${i}">C - ${e.alter[2]}</label>
+                <label id="correctionC_${i}"></label>
+            </div>
+
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="alterRadio-${i}" id="alterRadioD_${i}" value="${e.alter[3]}">
+                <label class="form-check-label" for="alterRadioD_${i}">D - ${e.alter[3]}</label>
+                <label id="correctionD_${i}"></label>
             </div>
     </div>
-    ` )
+    `
+
 });
+container_quest.innerHTML += `
+    <div class="text-center">
+        <input class="btn text-center text-light w-25" style="background-color: #EF7C00;" type="submit" value="Enviar" id="myInput" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    </div>`;
+
+
+//executa clicar ver resultado
+
+function startResult() {
+    let array_corect = []
+    let array_option = ["A","B","C","C"] 
+    newQuestions().forEach((e,i)=>{
+
+        const radion_A = getId(`alterRadioA_${i}`)
+        const radion_B = getId(`alterRadioB_${i}`)
+        const radion_C = getId(`alterRadioC_${i}`)
+        const radion_D = getId(`alterRadioD_${i}`)
+
+        const form_A = getId(`correctionA_${i}`)
+        const form_B = getId(`correctionB_${i}`)
+        const form_C = getId(`correctionC_${i}`)
+        const form_D = getId(`correctionD_${i}`)
+
+        let checkedd = null;
+        const checked = (radio,form) =>{
+            
+            if(radio.checked){
+                checkedd = radio.value
+                radio.disabled = true
+                if(checkedd === e.alter_correct){
+                    form.innerText = "✓"
+                    form.style.color = "green"
+                }
+            }else{
+                radio.disabled = true  
+                checkedd = radio.value
+                if(checkedd === e.alter_correct){
+                    form.innerText = "✕"
+                    form.style.color = "red"
+                }
+            }
+            
+        }
+
+        checked(radion_A,form_A)
+        checked(radion_B,form_B)
+        checked(radion_C,form_C)
+        checked(radion_D,form_D)
+
+
+       
+
+    })
+     //texto
+     const title = getId('title')
+     title.textContent = "RESULTADO"
+
+    console.log(array_corect)
+   
+}
 
 
 
 
-
-console.log(newQuestions()[0].alter[0])
+//console.log(newQuestions()[0].alter[0])
 
 
 
